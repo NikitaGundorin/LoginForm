1294f6afaa0dd469c4c19d7b6bae26850245979c
NikitaGundorin <djnig@yandex.ru>
2020-04-04 02:57:45 +0300

    Add UI tests

--- LoginFormUITests/LoginFormUITests.swift
+++ LoginFormUITests/LoginFormUITests.swift
@@ -10,34 +10,107 @@ import XCTest
 
 class LoginFormUITests: XCTestCase {
     var app: XCUIApplication!
-
+    
     override func setUpWithError() throws {
         app = XCUIApplication()
         app.launch()
         
         continueAfterFailure = false
     }
-
+    
     override func tearDownWithError() throws {
         app = nil
     }
-
-    func testSubmitButtonDisabled() throws {
-        
-        let app = XCUIApplication()
-        let passwordSecureTextField = app.secureTextFields["Password"]
-        passwordSecureTextField.tap()
-        passwordSecureTextField.tap()
-        app/*@START_MENU_TOKEN@*/.staticTexts["Submit"]/*[[".buttons[\"Submit\"].staticTexts[\"Submit\"]",".staticTexts[\"Submit\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
+    
+    func testSubmitButtonDisabledAfterLaunch() throws {
+        let isEnabled = app.buttons["Submit"].isEnabled
+        XCTAssertFalse(isEnabled)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testSubmitButtonEnablesAfterFillingTextFields() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("e")
+        let isEnabledAfterFillingEmail = app.buttons["Submit"].isEnabled
+        XCTAssertFalse(isEnabledAfterFillingEmail)
+        
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("p")
+        
+        let isEnabledAfterFillingPassword = app.buttons["Submit"].isEnabled
+        XCTAssertTrue(isEnabledAfterFillingPassword)
+    }
+    
+    func testSubmitButtonDisablesAfterRemovingTextFromEmailField() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("e")
+        
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("p")
+        
+        app.textFields["E-mail"].doubleTap()
+        app.keys["delete"].tap()
+        
+        let isEnabledAfterRemovingTextFromEmailField = app.buttons["Submit"].isEnabled
+        XCTAssertFalse(isEnabledAfterRemovingTextFromEmailField)
+    }
+    
+    func testSubmitButtonDisablesAfterRemovingTextFromPasswordField() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("e")
+        
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("p")
+        app.keys["delete"].tap()
+        
+        let isEnabledAfterRemovingTextFromPasswordField = app.buttons["Submit"].isEnabled
+        XCTAssertFalse(isEnabledAfterRemovingTextFromPasswordField)
+    }
+    
+    func testWhenSendWithInvalidEmailWarningIsShown() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("email.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("123")
+        app.buttons["Submit"].tap()
+        let warningLabel = app.staticTexts["Invalid email"]
+        XCTAssertTrue(warningLabel.exists)
+    }
+    
+    func testWhenSendWithInvalidPasswordWarningIsShoww() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("email@e.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("1")
+        app.buttons["Submit"].tap()
+        
+        let warningLabel1 = app.staticTexts["Password must not be shorter than 6 characters"]
+        XCTAssertTrue(warningLabel1.exists)
+        
+        app.secureTextFields["Password"].typeText("23456")
+        app.buttons["Submit"].tap()
+        let warningLabel2 = app.staticTexts["Password must contain at least one uppercase letter"]
+        XCTAssertTrue(warningLabel2.exists)
+        
+        app.secureTextFields["Password"].typeText("A")
+        app.buttons["Submit"].tap()
+        let warningLabel3 = app.staticTexts["Password must contain at least one lowercase letter"]
+        XCTAssertTrue(warningLabel3.exists)
+        
+        app.secureTextFields["Password"].doubleTap()
+        app.secureTextFields["Password"].typeText("Password")
+        app.buttons["Submit"].tap()
+        let warningLabel4 = app.staticTexts["Password must contain at least one number"]
+        XCTAssertTrue(warningLabel4.exists)
+    }
+    
+    func testWhenAllFieldsValidAndSendPressedWelcomeIsShown() {
+        app.textFields["E-mail"].tap()
+        app.textFields["E-mail"].typeText("email@e.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("Password1")
+        app.buttons["Submit"].tap()
+        
+        let welcomeStaticText = app.staticTexts["Welcome!"]
+        XCTAssertTrue(welcomeStaticText.exists)
     }
 }
